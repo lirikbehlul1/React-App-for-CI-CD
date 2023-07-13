c2b3bdf1fd7fcf44dedae69dd131b57b7c840af8
Lirik Behluli <lirik.behluli@polymaths.co>
2023-07-13 20:26:48 +0200

    Initial commit

--- ReactAppLirik.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// src/App.tsx
-var react_1 = require("react");
-var App = function () {
-    return <h1>Hello, World!</h1>;
-};
-module.exports = App;
--- ReactAppLirik.tsx
+++
@@ -1,9 +0,0 @@
-// src/App.tsx
-import React from 'react';
-
-const App: React.FC = () => {
-  return <h1>Hello, World!</h1>;
-};
-
-export = App;
-
